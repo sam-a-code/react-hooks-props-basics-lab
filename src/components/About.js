46879a883277870ff0c7d3f930bca3580dd50bf4
Sam Avery <sam.avery1@gmail.com>
2022-09-21 09:51:50 -0500

    commit tree ed8e769482d64e76ab135ec7273b4a6e42ebd1d3

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import user from "../data/user"
+import Links from "./Links";
 
-function About() {
+
+function About(user) {
+  const userBio = user.bio
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {userBio ? <p>{user.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links />
     </div>
   );
 }
